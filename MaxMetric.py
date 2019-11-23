b149b0537d7a35d7dd3cd5e77ecfce0e2de6104b
JanTry <44120531+WDIjantry@users.noreply.github.com>
2019-11-23 11:04:31 +0100

    Update MaxMetric.py

--- MaxMetric.py
+++ MaxMetric.py
@@ -1,8 +1,6 @@
 def distance(A,B):
     dx=A[0]-B[0]
     dy=A[1]-B[1]
-    return max(dx,-dx,dy,-dy)
-def isYCase(A,B):
     dx = A[0] - B[0]
     dy = A[1] - B[1]
     return max(dy,-dy)>max(dx,-dx)
