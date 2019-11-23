0d9494fed34df91ffa0b28b8d88d2c51690a73a5
JanTry <44120531+WDIjantry@users.noreply.github.com>
2019-11-23 19:07:22 +0100

    Update Type.py

--- .ipynb_checkpoints/Type.py
+++ .ipynb_checkpoints/Type.py
@@ -51,19 +51,20 @@ def rightEnd(point, p, rangeX, rangeY):
     return [fx, fy]
 
 
-def bisector(A, B, rangeX=[0, 100], rangeY=[0, 100]):
+def bisector(A, B, rangeX=[0, 1], rangeY=[0, 1]):
     dx = B[0] - A[0]
     dy = B[1] - A[1]
     dx = max(dx, -dx)
     dy = max(dy, -dy)
     if (dx > dy):
-        if (dy == 0):  # 1
-            return [[(A[0] + B[0]) / 2, rangeY[0]], [(A[0] + B[0]) / 2, rangeY[1]]]
-        # Linia jest w osi OY. I są punkty załamania
-        # [(D[0] + C[0]) / 2, C[1] - (dx / 2 - dy)], [(D[0] + C[0]) / 2, D[1] + (dx / 2 - dy)]
         if A[0] > B[0]:
             A[0], A[1], B[0], B[1] = B[0], B[1], A[0], A[1]
-        if A[1] < B[1]:  # 2
+        if (dy == 0):  # 1
+            return [[[(A[0] + B[0]) / 2, rangeY[0]], [(A[0] + B[0]) / 2, rangeY[1]]]]
+        # Linia jest w osi OY. I są punkty załamania
+        # [(D[0] + C[0]) / 2, C[1] - (dx / 2 - dy)], [(D[0] + C[0]) / 2, D[1] + (dx / 2 - dy)]
+
+        elif A[1] < B[1]:  # 2
             x = (B[0] + A[0]) / 2
             y1 = B[1] + (dx / 2 - dy)
             y2 = A[1] - (dx / 2 - dy)
@@ -72,15 +73,19 @@ def bisector(A, B, rangeX=[0, 100], rangeY=[0, 100]):
             return [[leftPoint, [x, y1]], [[x, y1], [x, y2]], [[x, y2], rightPoint]]
         else:  # 3
             x = (B[0] + A[0]) / 2
-            y2 = B[1] + (dx / 2 - dy)
-            y1 = A[1] - (dx / 2 - dy)
+            y1=min(A[1],B[1])
+            y2=max(A[1],B[1])
+            y2+=  (dx / 2 - dy)
+            y1-=  (dx / 2 - dy)
             leftPoint = leftEnd([x, y1], 1, rangeX, rangeY)
             rightPoint = rightEnd([x, y2], 1, rangeX, rangeY)
             return [[leftPoint, [x, y1]], [[x, y1], [x, y2]], [[x, y2], rightPoint]]
     elif dy > dx:
         if A[1] < B[1]:
             A[0], A[1], B[0], B[1] = B[0], B[1], A[0], A[1]
-        if A[0] < B[0]:  # 4
+        if (dx == 0):
+            return [[[rangeX[0],(A[1]+B[1])/2],[rangeX[1],(A[1]+B[1])/2]]]
+        elif A[0] < B[0]:  # 4
             y = (B[1] + A[1]) / 2
             x2 = B[0] + (dy / 2 - dx)
             x1 = A[0] - (dy / 2 - dx)
